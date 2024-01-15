0a1a8a8e32adcb29f18724c50e47b74df864ed11
shriyanshu raj <108788182+rajshriyanhu@users.noreply.github.com>
2024-01-15 17:37:24 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -322,9 +322,9 @@ const Home = () => {
   };
 
   if(loading){
-    return <div>
-      Loading!
-    </div>
+    return <div className="absolute top-0 left-0 w-full h-full backdrop-filter backdrop-blur-sm flex justify-center items-center bg-transparent bg-opacity-50">
+            <div className="animate-spin rounded-full h-20 w-20 border-t-4 border-blue-500"></div>
+        </div>
   }
 
   if(!verifiedUser){
